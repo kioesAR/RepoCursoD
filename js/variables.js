e4446823c0342030873b446f1f447cf0ea57f99e
egiovanetti <egiovanetti@gmail.com>
2024-05-19 21:40:18 -0300

    update clase

---
+++ js/variables.js
@@ -0,0 +1,21 @@
+var nombre = "Eze";
+var apellido = "Giova";
+
+console.log("Nombre:", nombre);
+console.log("Apellido:", apellido);
+
+var suma = nombre + " " + apellido;
+
+console.log("NombreApellido:", suma);
+
+var numero1 = 10;
+var numero2 = 20;
+
+var suma = numero1 + numero2;
+
+console.log("La suma es:", suma);
+
+var otroNum = 5;
+var otraVariable = suma + 5;
+
+console.log("La nueva variable es:", otraVariable);
